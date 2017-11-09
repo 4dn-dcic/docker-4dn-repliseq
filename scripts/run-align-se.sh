8e73829e1dd7dd37a89b6363f3a65aee30f6cda0
SooLee <duplexa@gmail.com>
2017-11-09 17:33:26 -0500

    added run-align-pe.sh, run-align-se.sh

---
+++ scripts/run-align-se.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+fastq1=$1
+index_file=$2
+outdir=$3
+prefix=$4
+nThreads=$5
+
+if [[ $outdir != '.' ]]
+then
+  cd $outdir
+fi
+
+# unzip index
+tar -xzf $index_file
+index=`ls -1 *.bwt | head -1 | sed 's/\.bwt//g'`
+
+# unzip fastq files
+if [[ $fastq1 =~ \.gz$ ]]
+then
+  cp $fastq1 fastq1.gz
+  gunzip fastq1.gz
+else
+  cp $fastq1 fastq1
+fi
+  fastq1=fastq1
+
+if [[ $fastq2 =~ \.gz$ ]]
+then
+  cp $fastq2 fastq2.gz
+  gunzip fastq2.gz
+else
+  cp $fastq2 fastq2
+fi
+  fastq2=fastq2
+
+
+# run bwa
+align_se $fastq1 $index . $prefix $nThreads
+
