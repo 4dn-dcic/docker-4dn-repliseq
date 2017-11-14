10c755d0831036b4546304230f998c9b5055acb0
Soo Lee <duplexa@gmail.com>
2017-11-14 17:57:46 -0500

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -10,10 +10,10 @@
 
 
 ## SOFTWARE: repli-seq-pipeline
-## COMMIT: fcfa351
+## COMMIT: 89dca8e
 git clone https://github.com/4dn-dcic/repli-seq-pipeline
 cd repli-seq-pipeline
-git checkout fcfa351
+git checkout 89dca8e
 rm -rf sample_data
 chmod +x *
 cd ..
