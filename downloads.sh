5c65e365cb646bc651b83778125af6ad584fbff0
Soo Lee <duplexa@gmail.com>
2017-12-08 12:51:14 -0500

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-## SOFTWARE: fastqc
-## VERSION: 0.11.5
-#wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.zip
-#unzip fastqc_v0.11.5.zip
-#cd FastQC/
-#chmod 755 fastqc
-#cd ..
-
-
 ## SOFTWARE: repli-seq-pipeline
 ## COMMIT: f2eb460
+## TYPE: aligner,filter,normalizer,smoother
 git clone https://github.com/4dn-dcic/repli-seq-pipeline
 cd repli-seq-pipeline
 git checkout f2eb460
@@ -21,11 +13,13 @@ cd ..
 
 ## SOFTWARE: cutadapt
 ## VERSION: 1.14
+## TYPE: adapter remover
 pip install cutadapt==1.14
 
 
 ## SOFTWARE: bwa
 ## VERSION: 0.7.15
+## TYPE: aligner
 wget https://github.com/lh3/bwa/archive/v0.7.15.tar.gz
 tar -xzf v0.7.15.tar.gz
 cd bwa-0.7.15
@@ -36,6 +30,7 @@ ln -s bwa-0.7.15 bwa
 
 ## SOFTWARE: bedtools
 ## VERSION: 2.26.0
+## TYPE: filter
 wget https://github.com/arq5x/bedtools2/releases/download/v2.26.0/bedtools-2.26.0.tar.gz
 tar -zxvf bedtools-2.26.0.tar.gz
 cd bedtools2
@@ -45,6 +40,7 @@ cd ..
 
 ## SOFTWARE: samtools
 ## VERSION: 1.4
+## TYPE: file format converter
 wget https://github.com/samtools/samtools/releases/download/1.4/samtools-1.4.tar.bz2
 tar -xjf samtools-1.4.tar.bz2
 cd samtools-1.4
