b295a57799cabe529a12d9b4c690ed98ef937ddf
SooLee <duplexa@gmail.com>
2018-04-04 17:08:02 -0400

    added test files

--- tests/tests_cwl_all.sh
+++ tests/tests_cwl_all.sh
@@ -1,4 +1,5 @@
-CWL_NAME_LIST=`ls -1 cwl/*cwl | sed 's/\.cwl//g' | sed 's/cwl\///g'`
+#CWL_NAME_LIST=`ls -1 cwl/*cwl | sed 's/\.cwl//g' | sed 's/cwl\///g'`
+CWL_NAME_LIST='repliseq-parta'
 for CWL_NAME in $CWL_NAME_LIST; do
        source tests/tests_cwl.sh $CWL_NAME;
 done
