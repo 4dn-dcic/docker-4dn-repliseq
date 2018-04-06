49c95d69ab69bc76a2ae3da730321d79227643f7
EC2 Default User <ec2-user@ip-172-31-48-253.ec2.internal>
2018-04-06 15:43:36 +0000

    fixed run-dedup

--- scripts/run-dedup.sh
+++ scripts/run-dedup.sh
@@ -15,12 +15,15 @@ QC_SUMMARY_FILE=${QCBASE}/summary.txt
 
 mkdir -p $QCBASE
 
-perl -ne 'if(m#\[bam_rmdupse_core] (\d+) / (\d+) = ([\d\.]+) in library#){ \
-            $tot=$1; $rm=$2; $prm=$3; } \
+# total mapped reads, in case no duplicate reads were detected
+TOT=`samtools view -F 0x4 $INPUT  | cut -f 1 | sort | uniq | wc -l`
+echo "Total aligned\t$TOT" > $QC_SUMMARY_FILE
+perl -ne 'if(m#\[bam_rmdupse_core] (\d+) / (\d+) = ([\d\.]+) in library#){
+            $rm=$2; $prm=$3; }
           else {
-            $tot=0; $rm=0; $prm=0; } \
-          print "Total aligned\t$tot\nNumber of removed duplicates\t$rm\nProportion of removed duplicates\t$prm\n"' \
+            $rm=0; $prm=0; }
+          print "Number of removed duplicates\t$rm\nProportion of removed duplicates\t$prm\n"' \
           $LOGFILE \
-          > $QC_SUMMARY_FILE
+          >> $QC_SUMMARY_FILE
 
-zip $QCBASE.zip $QCBASE
+zip -r $QCBASE.zip $QCBASE
