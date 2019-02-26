6b7b8395cb74fbd3d7f012916740b17d274ce7f1
Soo Lee <duplexa@gmail.com>
2019-02-26 16:11:33 -0500

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## SOFTWARE: repli-seq-pipeline
-## COMMIT: f69a459
+## COMMIT: 8584ff9
 ## TYPE: aligner,filter,normalizer,smoother
 ## SOURCE_URL: https://github.com/4dn-dcic/repli-seq-pipeline
 git clone https://github.com/4dn-dcic/repli-seq-pipeline
 cd repli-seq-pipeline
-git checkout f69a459
+git checkout 8584ff93
 rm -rf sample_data
 chmod +x *
 cd ..
